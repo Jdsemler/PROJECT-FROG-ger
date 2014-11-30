548805b1406606697351378b6a841476259a9bc8
Jdsemler <jdsemler@gmail.com>
2014-11-30 08:59:37 -0600

    Hop to it!
    FINALLY! We got the background panels complete! Just need to make the
    cars, make them move, and do collision detection for everything!

--- Hop to it!.py
+++ Hop to it!.py
@@ -2,15 +2,26 @@ from Tkinter import *
 root = Tk()
 # Create our drawpad and oval
 drawpad = Canvas(root, width=900,height=500, background='white')
-oval = drawpad.create_oval(40,40,80,80, fill="green")
-truck1 = drawpad.create_polygon(40, 40, 80, 80, fill="red")
-truck2 = drawpad.create_polygon(20,40,20,40)
+#background panels
+Grass1 = drawpad.create_rectangle (0,0,150,500, fill= 'chartreuse')
+Road1 = drawpad.create_rectangle (150,0,250,500, fill= 'grey')
+Grass2 = drawpad.create_rectangle (250,0,350,500, fill= 'chartreuse')
+Road2 = drawpad.create_rectangle (350,0,450,500, fill= 'grey')
+Grass3 = drawpad.create_rectangle (450,0,550,500, fill= 'chartreuse')
+Road4 = drawpad.create_rectangle (550,0,650,500, fill= 'grey')
+RoadDivider = drawpad.create_rectangle (650,0,650,500, fill= 'black')
+Road5 = drawpad.create_rectangle (550,0,750,500, fill= 'grey')
+pond = drawpad.create_rectangle (750,0,900,500, fill= 'blue')
 
+#character and enemies
+oval = drawpad.create_oval(40,40,80,80, fill='green')
+truck1 = drawpad.create_polygon(40, 40, 80, 80, fill='red')
+truck2 = drawpad.create_polygon(20,40,20,40, fill='blue')
 
 
 class MyApp:
 	def __init__(self, parent):
-	        # Make sure the drawpad is accessible from inside the function
+                # Make sure the drawpad is accessible from inside the function
 	        global drawpad
 		self.myParent = parent  
 		self.myContainer1 = Frame(parent)
@@ -26,10 +37,10 @@ class MyApp:
 		self.button2.configure(text="up", background= "yellow")
 		self.button2.grid(row=0,column=2)		
 		self.button2.bind("<Button-1>", self.button2Click)
-		
+			
 		self.button3 = Button(self.myContainer1)
 		self.button3.configure(text="down", background= "red")
-		self.button3.grid(row=0,column=3)		
+		self.button3.grid(row=0,column=3)	
 		self.button3.bind("<Button-1>", self.button3Click)
 		
 		self.button4 = Button(self.myContainer1)
@@ -69,6 +80,8 @@ class MyApp:
  		global oval
  		global drawpad
  		
+        	
 
+        
 myapp = MyApp(root)
 root.mainloop()
