b3f16e9281dc75a917377848b3c2ee1e4ac748d5
Jdsemler <jdsemler@gmail.com>
2014-12-02 16:00:44 -0600

    Hop to it ALMOST DONE!!!!!
    I made the trucks, got them moving, and made it so the frog cannot leave
    the screen. Just need to make collision detection for frog and trucks
    and make it so that when you hit the "pond" you win and the game resets.

--- Hop to it!.py
+++ Hop to it!.py
@@ -2,10 +2,8 @@ from Tkinter import *
 root = Tk()
 # Create our drawpad and oval
 drawpad = Canvas(root, width=900,height=500, background='white')
-frog = drawpad.create_oval(40,40,80,80, fill="green")
-oval = drawpad.create_oval(40,40,80,80, fill="green")
-truck1 = drawpad.create_polygon(40, 40, 80, 80, fill="red")
-truck2 = drawpad.create_polygon(20,40,20,40)
+oval = drawpad.create_oval(40,40,80,80, fill='green')
+
 
 #background panels
 Grass1 = drawpad.create_rectangle (0,0,150,500, fill= 'chartreuse')
@@ -14,15 +12,18 @@ Grass2 = drawpad.create_rectangle (250,0,350,500, fill= 'chartreuse')
 Road2 = drawpad.create_rectangle (350,0,450,500, fill= 'grey')
 Grass3 = drawpad.create_rectangle (450,0,550,500, fill= 'chartreuse')
 Road4 = drawpad.create_rectangle (550,0,650,500, fill= 'grey')
-
-RoadDivider = drawpad.create_rectangle (650,0,650,500, fill= 'black')
 Road5 = drawpad.create_rectangle (550,0,750,500, fill= 'grey')
 pond = drawpad.create_rectangle (750,0,900,500, fill= 'blue')
 
 #character and enemies
 oval = drawpad.create_oval(40,40,80,80, fill='green')
-truck1 = drawpad.create_polygon(40, 40, 80, 80, fill='red')
-truck2 = drawpad.create_polygon(20,40,20,40, fill='blue')
+
+truck = drawpad.create_rectangle(170,0,200,50, fill='blue')
+truck2 = drawpad.create_rectangle(370,0,400,50, fill='red')
+truck3 = drawpad.create_rectangle(570,0,600,50, fill= 'black')
+truck4 = drawpad.create_rectangle(700,0,730,50, fill= 'green')
+
+dir0 = 5
 
 
 class MyApp:
@@ -53,38 +54,68 @@ class MyApp:
 		self.button4.configure(text="right", background= "light blue")
 		self.button4.grid(row=0,column=4)		
 		self.button4.bind("<Button-1>", self.button4Click)
+ 		self.animate2()
  						
+        #truck movement
+        def animate2(self):
+            global dir0
+            global truck, truck2, truck3, truck4
+            # Get the x and y co-ordinates of the circle
+            x1, y1, x2, y2 = drawpad.coords(truck)
+            if y2 > drawpad.winfo_height(): 
+                dir0 = -6
+            elif y1 < 0:
+                dir0 = 6
+            #Move our trucks by the value of direction
+            drawpad.move(truck,0,dir0)
+            drawpad.move(truck2,0,dir0)
+            drawpad.move(truck3,0,dir0)
+            drawpad.move(truck4,0,dir0)
+            # Wait for 1 millisecond, then recursively call our animate function
+            drawpad.after(5, self.animate2)
+
+ 													
  		  
- 		# This creates the drawpad - no need to change this 
- 		drawpad.pack()		
+ 	# This creates the drawpad - no need to change this 
+ 	drawpad.pack()		
 
 	def button1Click(self, event):   
 		# Make the oval move to the left!
                 # "global" makes sure that we can access our oval and our drawpad
-		drawpad.move(oval,-20,0)
+		
 		global oval
 		global drawpad
+		x1,y1,x2,y2 = drawpad.coords(oval)
+		if x1 > 0:
+		    drawpad.move(oval,-20,0)
 			
  	def button2Click(self, event):   
  		# Make the oval move up!
                  # "global" makes sure that we can access our oval and our drawpad
-		drawpad.move(oval,0,-20)
- 		global oval
+		global oval
  		global drawpad
+ 		x1,y1,x2,y2 = drawpad.coords(oval)
+		if y1 > 0:
+		    drawpad.move(oval,0,-20)
  		
         def button3Click(self, event):   
  		# Make the oval move down!
                  # "global" makes sure that we can access our oval and our drawpad
-		drawpad.move(oval,0,20)
- 		global oval
+		global oval
  		global drawpad
+ 		x1,y1,x2,y2 = drawpad.coords(oval)
+		if y2 < 500:
+ 		     drawpad.move(oval,0,20)
  		
  	def button4Click(self, event):   
  		# Make the oval move to the right!
                  # "global" makes sure that we can access our oval and our drawpad
-		drawpad.move(oval,20,0)
- 		global oval
+		global oval
  		global drawpad
+ 		x1,y1,x2,y2 = drawpad.coords(oval)
+		if x1 < 900:
+ 		     drawpad.move(oval,20,0)
+ 		
  		
         	
 
